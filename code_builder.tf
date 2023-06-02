c63f2b217f0ec91df000128e7b83b38288c3d11f
stkraych <lichtenstein122@gmail.com>
2023-06-02 13:13:19 +0300

    code_builder files

--- code_builder.tf
+++ code_builder.tf
@@ -1,22 +1,24 @@
-resource "aws_codebuild_project" "my-codebuild_project" {
-  name          = "test-project"
-  description   = "test_codebuild_project"
-  build_timeout = "5"
-  service_role  = aws_iam_role.example.arn
+resource "aws_codebuild_project" "my_codebuild_project" {
+  name         = "my_codebuild_project"
+  description  = "my_codebuild_project"
+  service_role = aws_iam_role.eventbridge_codebuild_role.arn
+
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
   source {
     buildspec = templatefile("${path.module}/buildspec.yaml", {})
   }
 
-  # environment {
-  #   compute_type                = "BUILD_GENERAL1_SMALL"
-  #   image                       = "aws/codebuild/standard:1.0"
-  #   type                        = "LINUX_CONTAINER"
-  #   image_pull_credentials_type = "CODEBUILD"
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:1.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
 
-  #   environment_variable {
-  #     name  = "SOME_KEY1"
-  #     value = "SOME_VALUE1"
-  #   }
+
+  }
 
 }
 
