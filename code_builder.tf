5600406dd79643e957d668b1085c8c7163a1e7af
stkraych <lichtenstein122@gmail.com>
2023-06-02 14:06:53 +0300

    change codebuild project

--- code_builder.tf
+++ code_builder.tf
@@ -1,7 +1,7 @@
 resource "aws_codebuild_project" "my_codebuild_project" {
   name         = "my_codebuild_project"
   description  = "my_codebuild_project"
-  service_role = aws_iam_role.codebuilder_role.id
+  service_role = aws_iam_role.codebuilder_role.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
